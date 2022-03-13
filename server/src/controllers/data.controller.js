41d0905e2ac03ab09e94648c525c73d191ae810f
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-03-13 22:28:13 +0530

    data controller

--- server/src/controllers/data.controller.js
+++ server/src/controllers/data.controller.js
@@ -11,6 +11,24 @@ router.post("/", async(req, res) =>{
     }
 })
 
+router.get("/", async(req, res) => {
+    try {
+
+        let data;
+        let userid = req.query.user
+        if(userid) {
+            data = await Data.find({userId : userid})
+        }else {
+            data = await Data.find().lean().exec()
+        }        
+        
+        return res.status(200).send(data)
+
+    }catch(e){
+        return res.status(500).json({status: "Failed", message: e.message})
+    }
+})
+
 module.exports = router
 
 
