95f939edb6c241a9d43d8fcbb4df85520dc9c32f
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-03-14 14:25:06 +0530

    data input

--- client/src/components/DataInput.js
+++ client/src/components/DataInput.js
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import styles from '../styles/datainput.module.css'
 const DataInput = () => {
+  const [form, setForm] = useState({})
+
+  
   return (
-    <div>
-      
+    <div className={styles.mainInputBox}>
+      <h3>Enter Your Schedule data Here</h3>
+      <form>
+        <div>
+        <label for="title">Enter Title</label>
+        <input type="text" name='title' placeholder='Enter title here' required/>
+        <sup>*</sup>
+        </div>
+
+        <div>
+        <label for="scheduledDateTime">Select Time</label>
+        <input type="time" name='scheduledDateTime' required/>
+        <sup>*</sup>
+        </div>
+
+        <div>
+        <label>Enter Meet URL</label>
+        <input type="url"  name="link" placeholder='Optional(For Tasks)'/>
+        </div>
+
+        <div>
+          <button type='submit'>Save</button>
+        </div>
+      </form>
     </div>
   )
 }
