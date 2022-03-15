3d979bdc0262ec0f36f48847a039641d42eaf9df
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-03-15 23:39:57 +0530

    data input

--- client/src/components/DataInput.js
+++ client/src/components/DataInput.js
@@ -1,33 +1,66 @@
 import React, { useState } from 'react'
 import styles from '../styles/datainput.module.css'
+import Error from './Error'
 const DataInput = () => {
   const [form, setForm] = useState({})
+  const [error, setError] = useState(false)
+  const [errorMsg, setErrorMsg] = useState("")
 
-  
+  const handleInput = (e) => {
+    let {name, value} = e.target
+
+    setForm({
+      ...form,
+      [name] : value
+    })
+  }
+
+  const handleData = (e) => {
+    e.preventDefault()
+
+    let {title, scheduledDateTime, link} = form 
+
+    if(!title || !scheduledDateTime) {
+      setError(true)
+      setErrorMsg("Please fill valid title and schedule")
+      return
+    }
+
+  }
 
   return (
     <div className={styles.mainInputBox}>
       <h3>Enter Your Schedule data Here</h3>
-      <form>
+      {
+        error && 
+        <Error text={errorMsg}/>
+      }
+      <form className={styles.FormInput}>
         <div>
-        <label htmlFor="title">Enter Title</label>
-        <sup>*</sup>
-        <input type="text" name='title' placeholder='Enter title here' required/>
+        <span><label htmlFor="title">Enter Title</label>
+        <sup>*</sup></span>
+        <input type="text" name='title' placeholder='Enter title here' required
+        onChange={(e) => handleInput(e)}
+        />
         </div>
 
         <div>
-        <label htmlFor="scheduledDateTime">Select Time</label>
-        <sup>*</sup>
-        <input type="time" name='scheduledDateTime' required/>
+        <span><label htmlFor="scheduledDateTime">Select Time</label>
+        <sup>*</sup></span>
+        <input type="time" name='scheduledDateTime' required
+        onChange={(e) => handleInput(e)}
+        />
         </div>
 
         <div>
-        <label htmlFor='link'>Enter Meet URL</label>
-        <input type="url"  name="link" placeholder='Optional(For Tasks)'/>
+        <span><label htmlFor='link'>Enter Meet URL</label></span>
+        <input type="url"  name="link" placeholder='Optional(For Tasks)'
+        onChange={(e) => handleInput(e)}
+        />
         </div>
 
         <div>
-          <button type='submit'>Save</button>
+          <button type='submit' onClick={(e) => handleData(e)}>Save</button>
         </div>
       </form>
     </div>
