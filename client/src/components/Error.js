46312432f62921cdaa32c6b74c038c2aea0bed02
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-03-15 22:18:14 +0530

    error

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Error = ({text}) => {
   return (
-    <div>{text}</div>
+    <div style={{color:"#fff", 
+    backgroundColor:"orangered", width:"100%", 
+    textAlign:"center", padding: "5px", borderRadius:"5px"}}>{text}</div>
   )
 }
 
